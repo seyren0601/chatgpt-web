2517c980bafa3b6d6b5bac8bf2ef5b555864057e
ThinhNguyen <sorrenw@gmail.com>
2023-11-15 20:25:22 +0700

    Simple API (return "Hello World!") [/api/completion]

---
+++ ChatGPTCaller/Controllers/CompletionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ChatGPTCaller.Models;
+
+namespace ChatGPTCaller.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CompletionController : ControllerBase
+    {
+        string response = "Hello World!";
+
+        [HttpGet("completion")]
+        public string CompletionAPI()
+        {
+            return response;
+        }
+    }
+}
