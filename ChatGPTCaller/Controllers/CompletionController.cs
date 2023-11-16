1e51ac93ad8dea170a5eaff0b06ed6388b4c2333
ThinhNguyen <sorrenw@gmail.com>
2023-11-16 08:25:07 +0700

    POST API (http body là json có requestBody là prompt string)

--- ChatGPTCaller/Controllers/CompletionController.cs
+++ ChatGPTCaller/Controllers/CompletionController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ChatGPTCaller.Models;
+using System.Security.Cryptography.X509Certificates;
+using ChatGPTCaller.Services;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChatGPTCaller.Controllers
 {
@@ -8,12 +12,44 @@ namespace ChatGPTCaller.Controllers
     [ApiController]
     public class CompletionController : ControllerBase
     {
-        string response = "Hello World!";
+        public ChatGPT_API_Response.APIResponse Response { get; set; }
+        private readonly ChatGPTService _chatGPTService;
+        public CompletionController(ChatGPTService chatGPTService)
+        {
+            _chatGPTService = chatGPTService;
+        }
 
         [HttpGet("completion")]
-        public string CompletionAPI()
+        public ActionResult<string> Get()
         {
-            return response;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return "Hello World!";
+            }
+        }
+
+        [HttpPost("completion")]
+        public ActionResult<string> GetCompletionAPI([FromBody] Model model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                Response = _chatGPTService.GetAPIResponse(model.requestBody).Result;
+                return Response.choices[0].message.content;
+            }
+        }
+
+        public class Model
+        {
+            [Required]
+            public string requestBody { get; set; }
         }
     }
 }
