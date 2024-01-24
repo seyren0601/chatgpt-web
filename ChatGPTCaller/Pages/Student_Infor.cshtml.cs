bc7397d46a70b6f055cc8ac602e34dc9943d2007
ledanhthuong <ledanhthuong0909@gmail.com>
2024-01-24 23:06:21 +0700

    change the connection of css

---
+++ ChatGPTCaller/Pages/Student_Infor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChatGPTCaller.Pages
+{
+    public class Student_InforModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
