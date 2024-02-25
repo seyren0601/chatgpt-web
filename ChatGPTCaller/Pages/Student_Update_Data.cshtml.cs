d7e0f91ecb0c8c092a3ae915c2fe651f9074dacf
ledanhthuong <ledanhthuong0909@gmail.com>
2024-02-25 18:35:24 +0700

    add two pages for student information

---
+++ ChatGPTCaller/Pages/Student_Update_Data.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChatGPTCaller.Pages
+{
+    public class Student_Update_DataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
